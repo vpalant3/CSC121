3a20632a5d0c04abdf5e8d8282058cfbd52cae4f
vpalant3 <158368851+vpalant3@users.noreply.github.com>
2025-01-15 16:20:50 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Hello World
+//
+//  Created by Vladislav Palant on 1/14/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
