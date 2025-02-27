f0c20ec49c5ba8b4d788b9b2afb53a48d9de5f81
vpalant3 <158368851+vpalant3@users.noreply.github.com>
2025-02-27 12:16:35 -0600

    Update Adventure Story (Project).cpp

--- Adventure Story (Project).cpp
+++ Adventure Story (Project).cpp
@@ -25,7 +25,7 @@ int main(int argc, const char * argv[]) {
             if (cin.fail()) {
                 // The answer not integer I want
                 cin.clear();  // clear fail state
-                cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // discard rest words
                 cout << "Let's Follow the story! Please enter 1 or 2! ";
             }
             else if (Gender != 1 && Gender != 2) {
@@ -116,13 +116,13 @@ int main(int argc, const char * argv[]) {
                 if(SaveOrRun == 1) {
                     cout << endl << "You let the Wolf take your food and you got away safely!" << endl;
                     cout << "You end up back home and made more baked foods for yourself because you are scarred of the incident!" << endl;
-                    cout << "Do you want to play again?" << endl;
+                    cout << "Do you want to play again?(yes/no)" << endl;
                     cin >> playAgain;
                 } // saveorrun 1
                 else if (SaveOrRun == 2) {
                     cout << "You try fighting the wolf and the wolf eats you instead of the food!" << endl;
                     cout << "The food ends up rotting, stinking up the forest which cleans the forests of wolves" << endl;
-                    cout << "Do you want to play again?" << endl;
+                    cout << "Do you want to play again?(yes/no)" << endl;
                     cin >> playAgain;
                 } // SaveOrRun 2
             } // Closer of Dark Forest scenario?
@@ -180,7 +180,7 @@ int main(int argc, const char * argv[]) {
                     int disguisedwolf = rand() % 2;
                     if (disguisedwolf == 0) {
                         cout << "Your grandma opens it warm-heartedly happy to see you bring food to feel better!" << endl << "The End!" << endl;
-                        cout << "Do you want to play again?" << endl;
+                        cout << "Do you want to play again?(yes/no)" << endl;
                         cin >> playAgain;
                     } else {
                         cout << "You find the Wolf disguised as your grandma!" << endl << "The shock of finding a wolf dressed as your grandma startles you and you run away!" << endl;
@@ -190,23 +190,23 @@ int main(int argc, const char * argv[]) {
                 } // DoorOption closer
                 else if(DoorOption == 2){
                     cout << "You quietly leave the food at the back door, and quietly knock on the door as you slip away" << endl << "The End!" << endl;
-                    cout << "Do you want to play again?" << endl;
-                                cin >> playAgain;
+                    cout << "Do you want to play again?(yes/no)" << endl;
+                    cin >> playAgain;
                 } // Door Option 2 Closer
             } // Closes Wolf == 2?
         } // closes Gender = 1?
         
         if (Gender == 2) { // Girl Path (No Difference)
             srand(static_cast<unsigned>(time(nullptr)));//random maker
-                    
-        cout << "This is a story of a little girl with a little red cap" << endl;
-        cout << "You find out your grandma is sick, and want to bring her some baked foods!" << endl;
-        cout << "Since you know she is sick you want to get to her as soon as possible!" << endl;
-        cout << endl << "Which path do you take to get your grandma's" << endl; // Intro story
-                    
-        cout << "1) Take the dark, narrow path through the woods." << endl;
-        cout << "2) Take the long, scenic route along the river." << endl;
-                    int Wolf;
+            
+            cout << "This is a story of a little girl with a little red cap" << endl;
+            cout << "You find out your grandma is sick, and want to bring her some baked foods!" << endl;
+            cout << "Since you know she is sick you want to get to her as soon as possible!" << endl;
+            cout << endl << "Which path do you take to get your grandma's" << endl; // Intro story
+            
+            cout << "1) Take the dark, narrow path through the woods." << endl;
+            cout << "2) Take the long, scenic route along the river." << endl;
+            int Wolf;
             bool validWolf = false;
             while (!validWolf) {
                 cin >> Wolf;
@@ -223,10 +223,10 @@ int main(int argc, const char * argv[]) {
                     validWolf = true;
                 }
             }
-                    if (Wolf == 1) { // Dark forest Scenario
-                        cout << endl << "You were worried for your grandma and took down the quicker and narrow forest route!" << endl;
-                        cout << "As you run through the forest you trip and fall and all your baked foods fall!" << endl;
-                        cout << R"(                         ___________
+            if (Wolf == 1) { // Dark forest Scenario
+                cout << endl << "You were worried for your grandma and took down the quicker and narrow forest route!" << endl;
+                cout << "As you run through the forest you trip and fall and all your baked foods fall!" << endl;
+                cout << R"(                         ___________
                             __.-' `     `  ``--.___
                           .'.        `     `  `    `-.___
                          / -                   ` `  ` `  `--.
@@ -252,45 +252,44 @@ int main(int argc, const char * argv[]) {
          __.-.      --.        `-----.____    '._/__`._  ___   \=/   _/       _
             .--           -.  -                        `-   `------.--.__.---'
         )";
-            cout << "As you look up a big bad wolf stumbles in front of you growling with hunger..." << endl;
-            cout << "1) Do you give the food to the wolf and run away" << endl;
-            cout << "2) Do you fight the wolf to save the food for your Grandma" << endl;
-                        
-            int SaveOrRun;
-            bool validSaveOrRun = false;
-            while (!validSaveOrRun) {
-            cin >> SaveOrRun;
-            if (cin.fail()) {
-            // The answer not integer I want
-            cin.clear();  // clear fail state
-            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            cout << "Let's Follow the story! Please enter 1 or 2! ";
-            }
-            else if (SaveOrRun != 1 && SaveOrRun != 2) {
-            cout << "Invalid choice! Please enter 1 or 2: ";
-            }
-            else {
-            validSaveOrRun = true;
-            }
-        }
-            if(SaveOrRun == 1) {
-            cout << endl << "You let the Wolf take your food and you got away safely!" << endl;
-            cout << "You end up back home and made more baked foods for yourself because you are scarred of the incident!" << endl;
-            cout << "Do you want to play again?" << endl;
+                cout << "As you look up a big bad wolf stumbles in front of you growling with hunger..." << endl;
+                cout << "1) Do you give the food to the wolf and run away" << endl;
+                cout << "2) Do you fight the wolf to save the food for your Grandma" << endl;
+                
+                int SaveOrRun;
+                bool validSaveOrRun = false;
+                while (!validSaveOrRun) {
+                    cin >> SaveOrRun;
+                    if (cin.fail()) {
+                        // The answer not integer I want
+                        cin.clear();  // clear fail state
+                        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                        cout << "Let's Follow the story! Please enter 1 or 2! ";
+                    }
+                    else if (SaveOrRun != 1 && SaveOrRun != 2) {
+                        cout << "Invalid choice! Please enter 1 or 2: ";
+                    }
+                    else {
+                        validSaveOrRun = true;
+                    }
+                }
+                if(SaveOrRun == 1) {
+                    cout << endl << "You let the Wolf take your food and you got away safely!" << endl;
+                    cout << "You end up back home and made more baked foods for yourself because you are scarred of the incident!" << endl;
+                    cout << "Do you want to play again?(yes/no)" << endl;
                     cin >> playAgain;
-                        } // saveorrun 1
+                } // saveorrun 1
                 else if (SaveOrRun == 2) {
-            cout << "You try fighting the wolf and the wolf eats you instead of the food!" << endl;
-            cout << "The food ends up rotting, stinking up the forest which cleans the forests of wolves" << endl;
-            cout << "Do you want to play again?" << endl;
-                        cin >> playAgain;
-            } // SaveOrRun 2
-        } // Closer of Dark Forest scenario?
-                    
+                    cout << "You try fighting the wolf and the wolf eats you instead of the food!" << endl;
+                    cout << "The food ends up rotting, stinking up the forest which cleans the forests of wolves" << endl;
+                    cout << "Do you want to play again?(yes/no)" << endl;
+                    cin >> playAgain;
+                } // SaveOrRun 2
+            } // Closer of Dark Forest scenario?
             if(Wolf == 2) { // Long scenic route scenario
-            cout << endl << "You take the long scenic route taking your sweet time even though your grandma is sick!" << endl;
-            cout << "After taking your time walking through flowers and petals you reach to your Grandma's house!" << endl;
-                        cout << R"(
+                cout << endl << "You take the long scenic route taking your sweet time even though your grandma is sick!" << endl;
+                cout << "After taking your time walking through flowers and petals you reach to your Grandma's house!" << endl;
+                cout << R"(
                           /\                           (
                     .    /%%\                           )        /\
                    /"\  /%%%%\                         (__      /""\
@@ -315,48 +314,48 @@ int main(int argc, const char * argv[]) {
                    _-                                    '._.'._.'._.'._.'._.'._.'
                                                          ~"^"~"^~"^"~"^"~"^"~"^"~"    
             )";
-        cout << endl << "When you arrive do you..." << endl;
-        cout << "1) Knock on the front door" << endl;
-        cout << "2) Leave the food at the back door so you don't disturb your grandma" << endl;
-                        
-        int DoorOption;
-        bool validDoorOption = false;
-        while (!validDoorOption) {
-            cin >> DoorOption;
-            if (cin.fail()) {
-            // The answer not integer I want
-            cin.clear();  // clear fail state
-            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            cout << "Let's Follow the story! Please enter 1 or 2! ";
-            }
-            else if (DoorOption != 1 && DoorOption != 2) {
-            cout << "Invalid choice! Please enter 1 or 2: ";
-            }
-            else {
-            validDoorOption = true;
-            }
-        }
-                if (DoorOption == 1) {
-                            int disguisedwolf = rand() % 2;
-                            if (disguisedwolf == 0) {
-                                cout << "Your grandma opens it warm-heartedly happy to see you bring food to feel better!" << endl << "The End!" << endl;
-                                cout << "Do you want to play again?" << endl;
-                                cin >> playAgain;
-                            } else {
-                                cout << "You find the Wolf disguised as your grandma!" << endl << "The shock of finding a wolf dressed as your grandma startles you and you run away!" << endl;
-                                cout << "Do you want to play again?" << endl;
-                                cin >> playAgain;
-                            } // else closer
-                        } // DoorOption closer
-                        else if(DoorOption == 2){
-                            cout << "You quietly leave the food at the back door, and quietly knock on the door as you slip away" << endl << "The End!" << endl;
-                            cout << "Do you want to play again?" << endl;
-                                        cin >> playAgain;
-                        } // Door Option 2 Closer
-                    } // Closes Wolf == 2?
-                } // closes Gender = 1?
+                cout << endl << "When you arrive do you..." << endl;
+                cout << "1) Knock on the front door" << endl;
+                cout << "2) Leave the food at the back door so you don't disturb your grandma" << endl;
                 
-            } // Gender 2 closer
-            
+                int DoorOption;
+                bool validDoorOption = false;
+                while (!validDoorOption) {
+                    cin >> DoorOption;
+                    if (cin.fail()) {
+                        // The answer not integer I want
+                        cin.clear();  // clear fail state
+                        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                        cout << "Let's Follow the story! Please enter 1 or 2! ";
+                    }
+                    else if (DoorOption != 1 && DoorOption != 2) {
+                        cout << "Invalid choice! Please enter 1 or 2: ";
+                    }
+                    else {
+                        validDoorOption = true;
+                    }
+                }
+                if (DoorOption == 1) {
+                    int disguisedwolf = rand() % 2;
+                    if (disguisedwolf == 0) {
+                        cout << "Your grandma opens it warm-heartedly, happy to see you bring food for her to feel better!" << endl << "The End!" << endl;
+                        cout << "Do you want to play again?(yes/no)" << endl;
+                        cin >> playAgain;
+                    } else {
+                        cout << "You find the Wolf disguised as your grandma!" << endl << "The shock of finding a wolf dressed as your grandma startles you and you run away!" << endl;
+                        cout << "Do you want to play again?(yes/no)" << endl;
+                        cin >> playAgain;
+                    } // else closer
+                } // DoorOption closer
+                else if(DoorOption == 2){
+                    cout << "You quietly leave the food at the back door, and quietly knock on the door as you slip away" << endl << "The End!" << endl;
+                    cout << "Do you want to play again?(yes/no)" << endl;
+                    cin >> playAgain;
+                } // Door Option 2 Closer
+            } // Closes Wolf == 2?
+        } // closes Gender = 1?
+    } // Gender 2 closer
+    
+        cout << "Thank you for playing! Till next time!" << endl;
             return 0;
         }
